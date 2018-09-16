d578b526913da8bd247a508a10e500bd923d9a44
Chips0 <41867470+Chips0@users.noreply.github.com>
2018-09-16 23:30:54 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -342,4 +342,4 @@ var regToken = /[\w\d]{24}\.[\w\d]{6}\.[\w\d-_]{27}/g;
 //   console.log(chalk.bgBlue.green(e.replace(regToken, 'that was redacted')));
 // });
 
-client.login(ayarlar.token);
+client.login(process.env.BOTUN_TOKEN);
