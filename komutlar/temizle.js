b34d63b5926c5a7cb3a78c65d213d7c266ecbda6
Chips0 <41867470+Chips0@users.noreply.github.com>
2019-01-28 04:10:42 +0300

    Update temizle.js

--- komutlar/temizle.js
+++ komutlar/temizle.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 exports.run = function(client, message, args) {
-if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Bu Komutu Kullanmak İçin İzniniz Yok!");
+
 if(!args[0]) return message.channel.send("🚫 **Lütfen Silinicek Mesaj Miktarını Yazın.!** 🚫");
 message.channel.bulkDelete(args[0]).then(() => {
   message.channel.sendCode("asciicod", ` ✅ ${args[0]} Adet Mesaj Silindi. ✅`).then(msg => msg.delete(5000));
